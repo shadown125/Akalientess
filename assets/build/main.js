547fc75e0e9b905858028bfb0b54737fe1a5564a
YoLoN <shadyolown13@onet.pl>
2021-08-13 20:13:06 +0200

    Improve animations for mobile navigation

--- assets/build/main.js
+++ assets/build/main.js
@@ -1,1 +1,189 @@
-"use strict";(self.webpackChunksusanoo=self.webpackChunksusanoo||[]).push([[179],{1116:function(t,e,n){n(6992),n(1539),n(8783),n(8478),n(3948),n(4129);function i(t,e,n){if(!e.has(t))throw new TypeError("attempted to get private field on non-instance");return n}function a(t,e){return function(t,e){if(e.get)return e.get.call(t);return e.value}(t,o(t,e,"get"))}function s(t,e,n){return function(t,e,n){if(e.set)e.set.call(t,n);else{if(!e.writable)throw new TypeError("attempted to set read only private field");e.value=n}}(t,o(t,e,"set"),n),n}function o(t,e,n){if(!e.has(t))throw new TypeError("attempted to "+n+" private field on non-instance");return e.get(t)}var r=new WeakMap,c=new WeakMap,u=new WeakMap,l=new WeakSet,f=function t(e,n,o){!function(t,e){if(!(t instanceof e))throw new TypeError("Cannot call a class as a function")}(this,t),l.add(this),r.set(this,{writable:!0,value:void 0}),c.set(this,{writable:!0,value:void 0}),u.set(this,{writable:!0,value:void 0}),s(this,r,e),s(this,c,n),s(this,u,o),a(this,r).addEventListener("click",i(this,l,h).bind(this)),a(this,c).addEventListener("click",i(this,l,h).bind(this))};function h(){if(a(this,r).classList.contains("is-active"))return a(this,r).classList.remove("is-active"),void a(this,u).classList.remove("is-active");a(this,u).classList.add("is-active"),a(this,r).classList.add("is-active")}var d=new WeakSet;function v(){var t=document.querySelector("[data-button-close]"),e=document.querySelector("[data-button-menu]"),n=document.querySelector("[data-navigation-container]");new f(t,e,n)}new function t(){!function(t,e){if(!(t instanceof e))throw new TypeError("Cannot call a class as a function")}(this,t),d.add(this),function(t,e,n){if(!e.has(t))throw new TypeError("attempted to get private field on non-instance");return n}(this,d,v).call(this)}}},function(t){t.O(0,[736],(function(){return e=1116,t(t.s=e);var e}));t.O()}]);
+"use strict";
+(self["webpackChunksusanoo"] = self["webpackChunksusanoo"] || []).push([["main"],{
+
+/***/ "./src/scripts/Application.js":
+/*!************************************!*\
+  !*** ./src/scripts/Application.js ***!
+  \************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": function() { return /* binding */ Application; }
+/* harmony export */ });
+/* harmony import */ var core_js_modules_es_array_iterator_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! core-js/modules/es.array.iterator.js */ "./node_modules/core-js/modules/es.array.iterator.js");
+/* harmony import */ var core_js_modules_es_array_iterator_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(core_js_modules_es_array_iterator_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var core_js_modules_es_object_to_string_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! core-js/modules/es.object.to-string.js */ "./node_modules/core-js/modules/es.object.to-string.js");
+/* harmony import */ var core_js_modules_es_object_to_string_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(core_js_modules_es_object_to_string_js__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var core_js_modules_es_string_iterator_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! core-js/modules/es.string.iterator.js */ "./node_modules/core-js/modules/es.string.iterator.js");
+/* harmony import */ var core_js_modules_es_string_iterator_js__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(core_js_modules_es_string_iterator_js__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var core_js_modules_es_weak_set_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! core-js/modules/es.weak-set.js */ "./node_modules/core-js/modules/es.weak-set.js");
+/* harmony import */ var core_js_modules_es_weak_set_js__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(core_js_modules_es_weak_set_js__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var core_js_modules_web_dom_collections_iterator_js__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! core-js/modules/web.dom-collections.iterator.js */ "./node_modules/core-js/modules/web.dom-collections.iterator.js");
+/* harmony import */ var core_js_modules_web_dom_collections_iterator_js__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(core_js_modules_web_dom_collections_iterator_js__WEBPACK_IMPORTED_MODULE_4__);
+/* harmony import */ var _classes_NavigationEvents__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./classes/NavigationEvents */ "./src/scripts/classes/NavigationEvents.js");
+
+
+
+
+
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _classPrivateMethodGet(receiver, privateSet, fn) { if (!privateSet.has(receiver)) { throw new TypeError("attempted to get private field on non-instance"); } return fn; }
+
+
+
+var _initNavigationEvents = /*#__PURE__*/new WeakSet();
+
+var Application = function Application() {
+  _classCallCheck(this, Application);
+
+  _initNavigationEvents.add(this);
+
+  _classPrivateMethodGet(this, _initNavigationEvents, _initNavigationEvents2).call(this);
+};
+
+function _initNavigationEvents2() {
+  var closeButton = document.querySelector('[data-button-close]');
+  var menuButton = document.querySelector('[data-button-menu]');
+  var navigationContainer = document.querySelector('[data-navigation-container]');
+  var mobileContainerAnimation = document.querySelector('[data-mobile-container-animation]');
+  new _classes_NavigationEvents__WEBPACK_IMPORTED_MODULE_5__.NavigationEvents(closeButton, menuButton, navigationContainer, mobileContainerAnimation);
+}
+
+
+
+/***/ }),
+
+/***/ "./src/scripts/classes/NavigationEvents.js":
+/*!*************************************************!*\
+  !*** ./src/scripts/classes/NavigationEvents.js ***!
+  \*************************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "NavigationEvents": function() { return /* binding */ NavigationEvents; }
+/* harmony export */ });
+/* harmony import */ var core_js_modules_es_array_iterator_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! core-js/modules/es.array.iterator.js */ "./node_modules/core-js/modules/es.array.iterator.js");
+/* harmony import */ var core_js_modules_es_array_iterator_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(core_js_modules_es_array_iterator_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var core_js_modules_es_object_to_string_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! core-js/modules/es.object.to-string.js */ "./node_modules/core-js/modules/es.object.to-string.js");
+/* harmony import */ var core_js_modules_es_object_to_string_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(core_js_modules_es_object_to_string_js__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var core_js_modules_es_string_iterator_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! core-js/modules/es.string.iterator.js */ "./node_modules/core-js/modules/es.string.iterator.js");
+/* harmony import */ var core_js_modules_es_string_iterator_js__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(core_js_modules_es_string_iterator_js__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var core_js_modules_es_weak_map_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! core-js/modules/es.weak-map.js */ "./node_modules/core-js/modules/es.weak-map.js");
+/* harmony import */ var core_js_modules_es_weak_map_js__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(core_js_modules_es_weak_map_js__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var core_js_modules_web_dom_collections_iterator_js__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! core-js/modules/web.dom-collections.iterator.js */ "./node_modules/core-js/modules/web.dom-collections.iterator.js");
+/* harmony import */ var core_js_modules_web_dom_collections_iterator_js__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(core_js_modules_web_dom_collections_iterator_js__WEBPACK_IMPORTED_MODULE_4__);
+/* harmony import */ var core_js_modules_es_weak_set_js__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! core-js/modules/es.weak-set.js */ "./node_modules/core-js/modules/es.weak-set.js");
+/* harmony import */ var core_js_modules_es_weak_set_js__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(core_js_modules_es_weak_set_js__WEBPACK_IMPORTED_MODULE_5__);
+
+
+
+
+
+
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _classPrivateMethodGet(receiver, privateSet, fn) { if (!privateSet.has(receiver)) { throw new TypeError("attempted to get private field on non-instance"); } return fn; }
+
+function _classPrivateFieldGet(receiver, privateMap) { var descriptor = _classExtractFieldDescriptor(receiver, privateMap, "get"); return _classApplyDescriptorGet(receiver, descriptor); }
+
+function _classApplyDescriptorGet(receiver, descriptor) { if (descriptor.get) { return descriptor.get.call(receiver); } return descriptor.value; }
+
+function _classPrivateFieldSet(receiver, privateMap, value) { var descriptor = _classExtractFieldDescriptor(receiver, privateMap, "set"); _classApplyDescriptorSet(receiver, descriptor, value); return value; }
+
+function _classExtractFieldDescriptor(receiver, privateMap, action) { if (!privateMap.has(receiver)) { throw new TypeError("attempted to " + action + " private field on non-instance"); } return privateMap.get(receiver); }
+
+function _classApplyDescriptorSet(receiver, descriptor, value) { if (descriptor.set) { descriptor.set.call(receiver, value); } else { if (!descriptor.writable) { throw new TypeError("attempted to set read only private field"); } descriptor.value = value; } }
+
+var _closeButton = /*#__PURE__*/new WeakMap();
+
+var _menuButton = /*#__PURE__*/new WeakMap();
+
+var _navigationContainer = /*#__PURE__*/new WeakMap();
+
+var _mobileContainerAnimation = /*#__PURE__*/new WeakMap();
+
+var _addRemoveClass = /*#__PURE__*/new WeakSet();
+
+var NavigationEvents = function NavigationEvents(closeButton, menuButton, navigationContainer, mobileContainerAnimation) {
+  _classCallCheck(this, NavigationEvents);
+
+  _addRemoveClass.add(this);
+
+  _closeButton.set(this, {
+    writable: true,
+    value: void 0
+  });
+
+  _menuButton.set(this, {
+    writable: true,
+    value: void 0
+  });
+
+  _navigationContainer.set(this, {
+    writable: true,
+    value: void 0
+  });
+
+  _mobileContainerAnimation.set(this, {
+    writable: true,
+    value: void 0
+  });
+
+  _classPrivateFieldSet(this, _closeButton, closeButton);
+
+  _classPrivateFieldSet(this, _menuButton, menuButton);
+
+  _classPrivateFieldSet(this, _navigationContainer, navigationContainer);
+
+  _classPrivateFieldSet(this, _mobileContainerAnimation, mobileContainerAnimation);
+
+  _classPrivateFieldGet(this, _closeButton).addEventListener('click', _classPrivateMethodGet(this, _addRemoveClass, _addRemoveClass2).bind(this));
+
+  _classPrivateFieldGet(this, _menuButton).addEventListener('click', _classPrivateMethodGet(this, _addRemoveClass, _addRemoveClass2).bind(this));
+};
+
+function _addRemoveClass2() {
+  if (_classPrivateFieldGet(this, _closeButton).classList.contains('is-active')) {
+    _classPrivateFieldGet(this, _closeButton).classList.remove('is-active');
+
+    _classPrivateFieldGet(this, _navigationContainer).classList.remove('is-active');
+
+    _classPrivateFieldGet(this, _mobileContainerAnimation).classList.remove('is-active');
+
+    return;
+  }
+
+  _classPrivateFieldGet(this, _navigationContainer).classList.add('is-active');
+
+  _classPrivateFieldGet(this, _mobileContainerAnimation).classList.add('is-active');
+
+  _classPrivateFieldGet(this, _closeButton).classList.add('is-active');
+}
+
+/***/ }),
+
+/***/ "./src/scripts/main.js":
+/*!*****************************!*\
+  !*** ./src/scripts/main.js ***!
+  \*****************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _Application__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Application */ "./src/scripts/Application.js");
+
+new _Application__WEBPACK_IMPORTED_MODULE_0__.default();
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+/******/ __webpack_require__.O(0, ["vendor"], function() { return __webpack_exec__("./src/scripts/main.js"); });
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ }
+]);
--- assets/src/scripts/classes/NavigationEvents.js
+++ assets/src/scripts/classes/NavigationEvents.js
@@ -2,11 +2,13 @@ export class NavigationEvents {
     #closeButton;
     #menuButton;
     #navigationContainer;
+    #mobileContainerAnimation;
 
-    constructor(closeButton, menuButton, navigationContainer) {
+    constructor(closeButton, menuButton, navigationContainer, mobileContainerAnimation) {
         this.#closeButton = closeButton;
         this.#menuButton = menuButton;
         this.#navigationContainer = navigationContainer;
+        this.#mobileContainerAnimation = mobileContainerAnimation;
 
         this.#closeButton.addEventListener('click', this.#addRemoveClass.bind(this));
         this.#menuButton.addEventListener('click', this.#addRemoveClass.bind(this));
@@ -16,9 +18,11 @@ export class NavigationEvents {
         if (this.#closeButton.classList.contains('is-active')) {
             this.#closeButton.classList.remove('is-active');
             this.#navigationContainer.classList.remove('is-active');
+            this.#mobileContainerAnimation.classList.remove('is-active');
             return;
         }
         this.#navigationContainer.classList.add('is-active');
+        this.#mobileContainerAnimation.classList.add('is-active');
         this.#closeButton.classList.add('is-active');
     }
 }
