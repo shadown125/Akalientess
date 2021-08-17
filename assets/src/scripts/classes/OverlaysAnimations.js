b1daa8d72ef53365f5a8a8d3b6a7ffff1d05eee5
YoLoN <shadyolown13@onet.pl>
2021-08-17 16:16:35 +0200

    Add animation to navigation and social links

--- assets/src/scripts/classes/OverlaysAnimations.js
+++ assets/src/scripts/classes/OverlaysAnimations.js
@@ -1,10 +1,15 @@
 export class OverlaysAnimations {
 
     #rareOverlays;
+    #secondOverlays;
 
-    constructor(rareOverlays) {
+    constructor(rareOverlays, secondOverlays) {
         this.#rareOverlays = rareOverlays;
+        this.#secondOverlays = secondOverlays;
 
+        this.#secondOverlays.forEach((item) => {
+            this.#addSecondOverlayClass(item);
+        })
         this.#rareOverlays.forEach((item) => {
             this.#addOverlayClass(item);
         })
@@ -27,6 +32,22 @@ export class OverlaysAnimations {
                             entry.target.classList.add('is-revealed');
                         }, 1450)
                     }
+                })
+            }
+        };
+
+        const observer = new IntersectionObserver(onIntersection, {
+            threshold: 0.25
+        });
+
+        observer.observe(item);
+    }
+
+    #addSecondOverlayClass(item) {
+        const onIntersection = (entries) => {
+            if(entries[0]['intersectionRatio'] !== 0) {
+                entries.forEach((entry) => {
+                    entry.target.classList.add('is-revealed');
                 })
             }
         };
